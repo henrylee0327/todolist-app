bacbeb4093179faebeb574d8c86e083a782cb267
henry lee <djteski@gmail.com>
2020-05-14 21:09:46 -0500

    final commit

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const db = require('./lib/db')
+const validate = require('./lib/validate')
 
 const app = express()
 
 // serve files out of the public directory
 app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: true}))
 
 const port = 7878
 
@@ -51,8 +54,45 @@ app.get('/', function (req, res) {
 //The list page shows the items in the List
 app.get('/list/:listUUID', function (req, res) {
     const theList = req.todolist.list
-    res.render('list_page', {listName: theList.name, items: dummyItems})
+    res.render('list_page', {
+        listUUID: theList.uuid,
+        listName: theList.name, 
+        items: theList.items
+    })
 })
+
+// add a new item to a list
+app.post('/list/:listUUID/new-item', function (req, res) {
+    const theList = req.todolist.list
+    const newDescription = req.body.description
+        
+    // console.log(req)
+
+    if (validate.validDescription(newDescription)) {
+        // create the item
+        db.createItem(theList.id, newDescription)
+            .then((newItem) => {
+            res.render('item_created', {
+            listUUID: theList.uuid,
+            listName: theList.name, 
+            description: newItem.description
+                })
+            })
+            .catch(() => {
+                res.status(500).send('oh man, we totally messed up')
+            })
+        
+        // show them the success page
+        // res.status(201).send('you did good')
+    } else {
+        // show them an error page
+        res.status(400).send('bad input')
+    }
+
+    // const description = 'dummy new item'
+
+})
+
 
 const startExpressApp = () => {
     app.listen(port, () => {
